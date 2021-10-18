d370eaf3170016c7ba836eea48ce54ce62856651
Lakisha J <lakishajohnson@pursuit.org>
2021-10-18 11:08:15 -0400

    Add loop to response

--- todont.js
+++ todont.js
@@ -1,1 +1,12 @@
-console.log("Hey, everyone")
+const readlineSync = require("readline-sync")
+const getMenu = require("./getMenu")
+//ALL IMPORTS SHOULD BE AT TOP OF FILE W/SPACE BETWEEN FUNCTION
+//IF GET MENU IS CALLED A STRING IS RETURNED
+let response = readlineSync.question(getMenu())
+
+while (response !== "q") {
+    response = readlineSync.question(getMenu())
+}
+// console.log("you typed: " + response)
+
+
