cc4b962b5e2a1747ddfd7b7d74dfdd6e8efbea63
Lakisha J <lakishajohnson@pursuit.org>
2021-10-18 12:29:17 -0400

    Updated file dynamically

--- getMenu.js
+++ getMenu.js
@@ -1,14 +1,20 @@
-const getMenu = () => {
-    const menu = `Choose a todo to toggle its completeness (by entering its number) or choose an option at the bottom (by letter):
-1.  ✅ Walk the dog.
-2.  ❌ Do the dishes.
-3.  ✅  Call the bank.
-4.  ❌  Sign up for class.
-(l) View all lists.
-(q) Quit
-`
-return menu;
-};
+const chalk = require("chalk");
+const todos = require("./toDos");
+
+function getMenu() {
+    let number = 1; 
+    //{number++} OR {i + 1}
+  const menu = `${chalk.blue.bold(
+    "Choose a todo to toggle its completeness (by entering its number) or choose an option at the bottom (by letter):"
+  )}
+    ${todos.map((todo) => `${number++}. ${todo.text}`).join("\n")}
+    (l) View all lists.
+    (n) Add new todo.
+    (q) Quit
+    `;
+  return menu;
+}
+console.log(getMenu());
 
 module.exports = getMenu;
 
